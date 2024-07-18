8b19e1bd14720aefd8f56eddadc67b71d1984547
Zhao-Peng-Hui <71930169+Zhao-Peng-Hui@users.noreply.github.com>
2024-07-18 09:14:48 +0800

    Update recover_vless.py

--- vless/recover_vless.py
+++ vless/recover_vless.py
@@ -3,22 +3,6 @@ import json
 import subprocess
 import requests
 
-def send_telegram_message(token, chat_id, message):
-    telegram_url = f"https://api.telegram.org/bot{token}/sendMessage"
-    telegram_payload = {
-        "chat_id": chat_id,
-        "text": message,
-        "reply_markup": '{"inline_keyboard":[[{"text":"问题反馈❓","url":"https://t.me/yxjsjl"}]]}'
-    }
-
-    response = requests.post(telegram_url, json=telegram_payload)
-    print(f"Telegram 请求状态码：{response.status_code}")
-    print(f"Telegram 请求返回内容：{response.text}")
-
-    if response.status_code != 200:
-        print("发送 Telegram 消息失败")
-    else:
-        print("发送 Telegram 消息成功")
 
 # 从环境变量中获取密钥
 accounts_json = os.getenv('ACCOUNTS_JSON')
@@ -58,5 +42,3 @@ for server in servers:
     except subprocess.CalledProcessError as e:
         summary_message += f"\n无法恢复 {host} 上的 vless 服务：\n{e.output.decode('utf-8')}"
 
-# 发送汇总消息到 Telegram
-send_telegram_message(telegram_token, telegram_chat_id, summary_message)
